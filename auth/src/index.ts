89db18733608ff6beb07fccec003c8c7469001fa
zlatanovics1 <office@zlatanovics.com>
2024-07-03 11:53:31 +0200

    Added Auth Deployment Config in k8s

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import "express-async-errors";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -7,22 +9,29 @@ import { currentUserRouter } from "./routes/currentUser";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/errorHandler";
 import { NotFoundError } from "./errors/notFoundError";
-import mongoose from "mongoose";
 
 const app = express();
 
 (async function () {
   try {
+    if (!process.env.JWT_SECRET) throw new Error("JWT SECRET not defined");
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
   } catch (err) {
     console.error(err);
   }
 })();
+app.set("trust proxy", true);
 
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(currentUserRouter);
 app.use(signupRouter);
+app.use(
+  cookieSession({
+    secure: true,
+    signed: false,
+  })
+);
 
 app.all("*", () => {
   throw new NotFoundError();
