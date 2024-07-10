c07f04a6aaa397ebd07cd00cebcf16964ce087a4
zlatanovics1 <office@zlatanovics.com>
2024-07-10 23:39:03 +0200

    Added tests for /api/tickets post requests

--- tickets/routes/__test__/new.test.ts
+++ tickets/routes/__test__/new.test.ts
@@ -1,16 +1,30 @@
 import request from "supertest";
 import { app } from "./../../app";
 
-it("has route handler for ticketing service: /api/tickets", async () => {
+it("has route handler for creating tickets", async () => {
   const response = await request(app).post("/api/tickets").send({});
 
   expect(response.status).not.toEqual(404);
 });
 
-it("only authenticated users can access tickets service", async () => {});
+it("only authenticated users can create a ticket", async () => {
+  await request(app).post("/api/tickets").send({}).expect(401);
+});
 
-it("returns an error when invalid title is provided", async () => {});
+it("returns an error when invalid title is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({ title: "", price: 10 })
+    .expect(400);
+});
 
-it("returns an error when invalid price is provided", async () => {});
+it("returns an error when invalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({ title: "New title", price: -10 })
+    .expect(400);
+});
 
 it("creates a new ticket with valid input", async () => {});
